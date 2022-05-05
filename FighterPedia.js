cf9f3a18fb63abc8b3c89e22316bda4c7950d1d4
Søren Snegl <sorenlykkeberg@gmail.com>
2022-05-05 10:31:39 +0200

    Fyldte createFighter funktionen

--- FighterPedia.js
+++ FighterPedia.js
@@ -4,7 +4,8 @@ class FighterPedia extends SQLBase{
     constructor(connection){
         super(connection);
     }
-createFighter(fighterid) {
+    createFighter(fighterId, navn, price, rarity) {
+        return this.makeQuery(`INSERT INTO FighterPedia(id, navn, billede, moves, types, stats, price, rarity) VALUES(${fighterId}, "${navn}, BLOB, JSON, JSON, JSON, ${price}, ${rarity});`);
+    }
     
-}
 }
